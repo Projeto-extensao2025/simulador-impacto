330dc7340afa26629f4922f1f8caf8544da3c23e
Projeto-extensao2025 <flavioemaiara@gmail.com>
2026-05-09 17:24:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,29 +14,24 @@ function atualizar() {
   let barra = document.getElementById("progresso");
   let estado = document.getElementById("estado");
   let msg = document.getElementById("mensagem");
-  let body = document.body;
 
-  // barra
+  if (!barra || !estado || !msg) {
+    console.log("Elementos não encontrados");
+    return;
+  }
+
   barra.style.width = impacto + "%";
 
   if (impacto <= 30) {
     estado.innerText = "🌿 Ambiente saudável";
-    msg.innerText = "Suas escolhas estão ajudando o meio ambiente!";
-    barra.style.background = "green";
-    body.style.background = "linear-gradient(green, #0b3d0b)";
-  }
-
+    msg.innerText = "Suas escolhas estão ajudando!";
+  } 
   else if (impacto <= 70) {
-    estado.innerText = "⚠️ Situação de atenção";
-    msg.innerText = "Pequenas atitudes estão gerando impacto.";
-    barra.style.background = "orange";
-    body.style.background = "linear-gradient(#a67c00, #5c4300)";
-  }
-
+    estado.innerText = "⚠️ Atenção";
+    msg.innerText = "Pequenas ações acumulam impacto.";
+  } 
   else {
-    estado.innerText = "🔥 Impacto crítico";
-    msg.innerText = "O ambiente está sendo degradado rapidamente!";
-    barra.style.background = "red";
-    body.style.background = "linear-gradient(#660000, black)";
+    estado.innerText = "🔥 Crítico";
+    msg.innerText = "O ambiente está sendo degradado.";
   }
 }
